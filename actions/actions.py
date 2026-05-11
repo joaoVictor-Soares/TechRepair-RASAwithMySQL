d354e3121be1c5c0ea9bb0dceb46328692537a47
JOÃO VICTOR DA SILVA NEVES SOARES <joao.soares81@senaisp.edu.br>
2026-05-11 20:27:14 -0300

    criação do banco e incício dos rasas

---
+++ actions/actions.py
@@ -0,0 +1,8 @@
+from connector_db import get_db_connection
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+
+from rasa_sdk.executor import CollectingDispatcher
+
---
+++ db/banco_de_dados.sql
@@ -0,0 +1,134 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tech_repair_db
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dispositivos`
+--
+
+DROP TABLE IF EXISTS `dispositivos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dispositivos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cliente_id` int NOT NULL,
+  `modelo` varchar(100) NOT NULL,
+  `marca` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cliente` (`cliente_id`),
+  CONSTRAINT `fk_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dispositivos`
+--
+
+LOCK TABLES `dispositivos` WRITE;
+/*!40000 ALTER TABLE `dispositivos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dispositivos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ordens_servico`
+--
+
+DROP TABLE IF EXISTS `ordens_servico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ordens_servico` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dispositivo_id` int NOT NULL,
+  `tecnicos_id` int NOT NULL,
+  `descricao_problema` text NOT NULL,
+  `status` varchar(30) NOT NULL DEFAULT 'Pendente',
+  `data_abertura` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_tecnicos` (`tecnicos_id`),
+  KEY `fk_dispositivos` (`dispositivo_id`),
+  CONSTRAINT `fk_dispositivos` FOREIGN KEY (`dispositivo_id`) REFERENCES `dispositivos` (`id`),
+  CONSTRAINT `fk_tecnicos` FOREIGN KEY (`tecnicos_id`) REFERENCES `tecnicos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ordens_servico`
+--
+
+LOCK TABLES `ordens_servico` WRITE;
+/*!40000 ALTER TABLE `ordens_servico` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ordens_servico` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tecnicos`
+--
+
+DROP TABLE IF EXISTS `tecnicos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tecnicos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `especialidade` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tecnicos`
+--
+
+LOCK TABLES `tecnicos` WRITE;
+/*!40000 ALTER TABLE `tecnicos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tecnicos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-11 19:45:14
